19e670197a083b48ef70f5fbf2142e799ba14616
Artem <artem.muronenko@ukr.net>
2019-07-31 14:19:09 +0300

    1

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
-
+    public Vector2 v;
+    public Vector2 Dv;
     public float speed;
+
+    public int NumbMeth;
     // Use this for initialization
     void Start()
     {
+        v = transform.position;
         speed = Random.Range(0.2f, 0.4f);
+        NumbMeth = Random.Range(0, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * speed * Time.deltaTime;
+        if (NumbMeth == 0)
+            Forvard(this.gameObject);
+        else
+            Rotate(this.gameObject);
+    }
+
+    void Forvard(GameObject gameObject)
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    void Rotate(GameObject gameObject)
+    {
+        gameObject.transform.position += transform.up * speed * Time.deltaTime;
+        if (Time.time != 0)
+            Dv = (new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) - v) / Time.time;
+        gameObject.transform.Rotate(Dv);
     }
 }
