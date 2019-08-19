e90ed3141becca5a3b2e15504ce04dd2febac3b2
Artem <artem.muronenko@ukr.net>
2019-08-19 19:16:34 +0300

    1

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
+
     public Vector2 v;
     public Vector2 Dv;
     public float speed;
@@ -19,70 +20,104 @@ public class Plane : MonoBehaviour
     {
         v = transform.position;
         speed = Random.Range(0.2f, 0.4f);
-        NumbMeth = Random.Range(0, 1);
-
-        
+        NumbMeth = Random.Range(0, 4);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         time += Time.deltaTime;
-
-        Rocket = GameObject.FindGameObjectWithTag("Rocket");
-        if (Rocket == true)
-        {
-            float dist = Vector3.Distance(transform.position, Rocket.transform.position);
-
-        }
+                
         if (NumbMeth == 0)
-            Forvard(this.gameObject);
-        else
-            Rotate(this.gameObject);
+            Forvard();
+        if (NumbMeth == 1)
+            ForvardUp();
+        if (NumbMeth == 2)
+            ForvardDown();
+        if (NumbMeth == 3)
+            Sinus();
+        if (NumbMeth == 4)
+            Center();
     }
 
-    void Forvard(GameObject gameObject)
+    void Forvard()
     {
-        //transform.Translate(Vector3.up * Time.deltaTime * speed);
+        transform.Translate(new Vector2(0, 1) * Time.deltaTime * speed);
         //transform.position += transform.up * speed * Time.deltaTime;
-        Vector2 v2 = transform.position;
-        v2.x += speed * Time.deltaTime;        
-        
-        gameObject.transform.position = new Vector3(v2.x, v2.y, 0);
+        //Vector2 v2 = transform.position;
+        //v2.x += speed * Time.deltaTime;
 
-        
+        //transform.position = new Vector3(v2.x, v2.y, 0);
     }
 
-    void ForvardUp(GameObject gameObject)
+    void ForvardUp()
     {
         //transform.Translate(Vector3.up * Time.deltaTime * speed);
         //transform.position += transform.up * speed * Time.deltaTime;
         Vector2 v2 = transform.position;
         v2.x += speed * Time.deltaTime;
         v2.y += speed * Time.deltaTime;
-
-        gameObject.transform.position = new Vector3(v2.x, v2.y, 0);
-
-
+        transform.rotation = Quaternion.Euler(0, 0, RotationGradWithRotat(v2, this.gameObject));
+        transform.position = new Vector3(v2.x, v2.y, 0);
     }
-    void ForvardDown(GameObject gameObject)
+    void ForvardDown()
     {
         //transform.Translate(Vector3.up * Time.deltaTime * speed);
         //transform.position += transform.up * speed * Time.deltaTime;
         Vector2 v2 = transform.position;
         v2.x += speed * Time.deltaTime;
         v2.y -= speed * Time.deltaTime;
-
-        gameObject.transform.position = new Vector3(v2.x, v2.y, 0);
-
+        transform.rotation = Quaternion.Euler(0, 0, RotationGradWithRotat(v2, this.gameObject));
+        transform.position = new Vector3(v2.x, v2.y, 0);
 
     }
 
-    void Rotate(GameObject gameObject)
+    void Rotate()
     {
         gameObject.transform.position += transform.up * speed * Time.deltaTime;
         if (Time.time != 0)
             Dv = (new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) - v) / Time.time;
         gameObject.transform.Rotate(Dv);
+    }
+
+    void Center()
+    {
+        Vector3[] point = new Vector3[] { new Vector3(0, 0, 0), new Vector3(5, 3, 0) };
+        //float radius = Vector3.Distance(gameObject.transform.position, new Vector3(0, 0, 0));
+        //float a = Mathf.Pow(speed, 2) / radius;
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        Vector3 target = point[0] - transform.position;
+        Quaternion direct = Quaternion.LookRotation(target);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(RotationGrad(point[0], this.gameObject), Vector3.forward), speed * Time.deltaTime);
+    }
+
+    void Sinus()
+    {
+        Vector2 pos = new Vector2();
+
+        pos.x = transform.position.x + speed * Time.deltaTime;
+        pos.y = transform.position.y + Mathf.Sin(pos.x) * Time.deltaTime;
+        
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(RotationGradWithRotat(pos, this.gameObject), Vector3.forward), 1);
+        transform.position = pos;
+    }
+
+
+    float RotationGrad(Vector2 pos, GameObject plane)//поворот в строну объекта
+    {
+        float dX = pos.x - plane.transform.position.x;
+        float dY = pos.y - plane.transform.position.y;
+
+        float rot = Mathf.Atan2(dY, dX) * Mathf.Rad2Deg;                  //перевод радиан в градуси и -90 поворот спрайта
+        return rot;
+    }
+    float RotationGradWithRotat(Vector2 pos, GameObject plane)//поворот в строну объекта
+    {
+        float dX = pos.x - plane.transform.position.x;
+        float dY = pos.y - plane.transform.position.y;
+
+        float rot = Mathf.Atan2(dY, dX) * Mathf.Rad2Deg - 90;                  //перевод радиан в градуси и -90 поворот спрайта
+        return rot;
     }
 }
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -11,49 +11,139 @@ public class Rocket : MonoBehaviour
     public GameObject Point;
 
     Vector2 pos;
+    Vector2 posR;
 
     float time = 0;
 
     void Start()
     {
         Plane = GameObject.FindGameObjectWithTag("Plane");
-        float dist = Vector3.Distance(transform.position, Plane.transform.position);
-        Vector3 pos = Plane.transform.position;
-        pos.x = Plane.transform.position.x + (dist * Plane.GetComponent<Plane>().speed);
-        Instantiate(Point, pos, Quaternion.identity);
+        posR = transform.position;
+        SinusPoint();
 
-        Debug.Log(Plane.transform.position.x * Time.time + " " + Time.time + " " + dist + " " + Plane.GetComponent<Plane>().speed);        
+        //Debug.Log(Plane.transform.position.x * Time.time + " " + Time.time + " " + dist + " " + Plane.GetComponent<Plane>().speed);        
     }
-
     void Update()
     {
         time += Time.deltaTime;
 
         Plane = GameObject.FindGameObjectWithTag("Plane");
 
-        transform.rotation = Quaternion.Euler(0,0, RotationGrad(Plane,this.gameObject));
-       
+        transform.rotation = Quaternion.Euler(0, 0, RotationGrad(Plane, this.gameObject));
         transform.position = Vector3.MoveTowards(transform.position, Plane.transform.position, Time.deltaTime * speed);
-        if (Vector3.Distance(transform.position, Plane.transform.position) <= 0.5f)
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {        
+        if (other.gameObject.tag == "Plane")
         {
-            pos.x = Random.Range(-10, 10);
-            pos.y = Random.Range(-10, 10);
+            pos.x = Random.Range(-24, 24);
+            pos.y = Random.Range(-12, 12);
 
             Destroy(gameObject);
             Destroy(Plane);
-            Debug.Log(time + " " + Time.time + " " + Plane.transform.position);
+            Point = GameObject.FindGameObjectWithTag("Point");
+            Destroy(Point);
             Instantiate(PlaneCopy, pos, Quaternion.Euler(0, 0, -90));
 
             time = 0;
         }
     }
 
-    float RotationGrad(GameObject plane,GameObject rocket)
+    void ForvardPoint()
+    {
+        float dist = Vector3.Distance(transform.position, Plane.transform.position);
+        float t = dist / (speed + Plane.GetComponent<Plane>().speed);
+        float S = t * Plane.GetComponent<Plane>().speed + Plane.transform.position.x;
+
+        Vector3 pos;
+        pos.x = S;
+        pos.y = Plane.transform.position.y;
+        pos.z = 0;
+
+        Instantiate(Point, pos, Quaternion.identity);
+
+    }
+
+    void ForvardUpPoint()
+    {
+        float dist = Vector3.Distance(transform.position, Plane.transform.position);
+        float t = dist / (speed + Plane.GetComponent<Plane>().speed);
+        float S = t * Plane.GetComponent<Plane>().speed + Plane.transform.position.x;
+
+        Debug.Log(dist);
+
+        Vector2 v;
+        v.x = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.x;
+        v.y = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.y;
+
+
+
+        dist = dist + Mathf.Abs(dist - Vector3.Distance(new Vector2(0, 0), v));
+
+        t = dist / (speed + Plane.GetComponent<Plane>().speed);
+
+        v.x = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.x;
+        v.y = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.y;
+        Debug.Log(dist);
+
+        Instantiate(Point, v, Quaternion.identity);
+    }
+    void SinusPoint()
+    {
+        float dist = Vector3.Distance(transform.position, Plane.transform.position);
+        float t = dist / (speed + Plane.GetComponent<Plane>().speed);
+        float S = t * Plane.GetComponent<Plane>().speed + Plane.transform.position.x;
+
+        Vector3 pos;
+        pos.x = S;
+        pos.y = Plane.transform.position.y + Mathf.Sin(pos.x) * Time.deltaTime;
+        pos.z = 0;
+
+        Debug.Log(t);
+
+        Instantiate(Point, pos, Quaternion.identity);
+    }
+    void ForvardDowPoint()
+    {
+        float dist = Vector3.Distance(transform.position, Plane.transform.position);
+        float t = dist / (speed + Plane.GetComponent<Plane>().speed);
+        float S = t * Plane.GetComponent<Plane>().speed + Plane.transform.position.x;
+
+        Vector2 v;
+        v.x = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.x;
+        v.y = Plane.transform.position.y - (t * Plane.GetComponent<Plane>().speed);
+
+        dist = dist + Mathf.Abs(dist - Vector3.Distance(new Vector2(0, 0), v));
+
+        t = dist / (speed + Plane.GetComponent<Plane>().speed);
+
+        v.x = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.x;
+        v.y = Plane.transform.position.y - (t * Plane.GetComponent<Plane>().speed);
+
+        Instantiate(Point, v, Quaternion.identity);
+    }
+
+    void CenterPoint()
+    {
+        float dist = Vector3.Distance(transform.position, Plane.transform.position);
+        float t = dist / (speed + Plane.GetComponent<Plane>().speed);
+        float S = t * Plane.GetComponent<Plane>().speed + Plane.transform.position.x;
+
+        Debug.Log(dist);
+
+        Vector2 v;
+        v.x = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.x;
+        v.y = (t * Plane.GetComponent<Plane>().speed) + Plane.transform.position.y;
+
+        Instantiate(Point, v, Quaternion.identity);
+    }
+    float RotationGrad(GameObject plane, GameObject rocket)//поворот в строну объекта
     {
         float dX = plane.transform.position.x - rocket.transform.position.x;
         float dY = plane.transform.position.y - rocket.transform.position.y;
 
-        float rot = Mathf.Atan2(dY, dX) * 180 / Mathf.PI - 55;                  //перевод радиан в градуси и -55 поворот спрайта
+        float rot = Mathf.Atan2(dY, dX) * Mathf.Rad2Deg - 55;                  //перевод радиан в градуси и -55 поворот спрайта
         return rot;
     }
 }
